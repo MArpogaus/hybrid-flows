8a7a044cbafa3f0c327c260c9cf698b122ae97ee
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2023-09-15 16:49:54 +0200

    removes push-job

--- mlflow_command.sh
+++ mlflow_command.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -ux
 
-dvc remote add --local local /data/mctm/
+dvc remote add --force --local local /data/mctm/
 dvc pull -r local
 
 set -eu
 
 dvc exp run $@
-git add dvc.lock
-gc -m "updates dvc.lock from cml runner"
 
 dvc push -r local
