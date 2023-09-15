b7226fdfcc23d822c891b12d590eb9e20a73a593
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2023-09-15 13:08:37 +0200

    separates ci into several stages

--- mlflow_command.sh
+++ mlflow_command.sh
@@ -11,9 +11,3 @@ git add dvc.lock
 gc -m "updates dvc.lock from cml runner"
 
 dvc push -r local
-
-git config --global user.email "${GITLAB_USER_EMAIL}"
-git config --global user.name "${GITLAB_USER_NAME}"
-
-git remote set-url --push origin git@gitlab:${CI_PROJECT_PATH}
-git push --follow-tags origin HEAD:${CI_COMMIT_REF_NAME}
