149094259d3ef38c0599dcdfe455da112481c3c4
Marcel Arpogaus <marcel.arpogaus@htwg-konstanz.de>
2023-10-03 14:06:31 +0000

    reproduced pipeline for simulated data

--- hp_benchmark.sh
+++ hp_benchmark.sh
@@ -17,10 +17,10 @@ datasets="
 get_params(){
 
     if [ "$2" == "masked_autoregressive_bernstein_flow" ]; then
-        echo "-S $1.$2.$3.fit_kwds.batch_size=512,1024
-          -S $1.$2.$3.fit_kwds.learning_rate=0.01,0.05,0.001,0.005
-          -S $1.$2.$3.fit_kwds.lr_patience=5,10,15,20"
-        echo "-S $1.$2.$3.parameter_kwds.hidden_units=[16,16],[16,16,16],[512,512]"
+        echo "-S $1.$2.$3.fit_kwds.batch_size=128,512
+          -S $1.$2.$3.fit_kwds.learning_rate=0.01,0.001
+          -S $1.$2.$3.fit_kwds.lr_patience=5,15
+          -S $1.$2.$3.parameter_kwds.hidden_units=[64,64,64],[512,512,512]"
     else
         echo "-S $1.$2.$3.fit_kwds.batch_size=512,1024,2048
           -S $1.$2.$3.fit_kwds.learning_rate=0.01,0.001,0.0001
@@ -32,7 +32,12 @@ get_params(){
 # clean dvc queue
 dvc queue stop --kill
 dvc queue remove --all
+
 rm -r results/ || echo "results not present" # to remove old artifacts
+
+# reproduce pipeline to cache results
+dvc repro
+
 #rm my_joblog.log || echo "logfile not present" # used by parallel
 counter=0
 for stage in "${!models[@]}"; do
@@ -47,32 +52,30 @@ for stage in "${!models[@]}"; do
     done
 done
 
-#[ ! $(pgrep mlflow) ] && mlflow ui &
-#export MLFLOW_TRACKING_URI=http://127.0.0.1:5000
+[ ! $(pgrep mlflow) ] && mlflow ui &
+export MLFLOW_TRACKING_URI=http://127.0.0.1:5000
 
 echo "executing experiments"
-while true; do
-  status="$(dvc queue status | head -n-2 | tail -n+2)"
-  status=$(echo "$status" | grep "Queued")
+# while true; do
+#   status="$(dvc queue status | head -n-2 | tail -n+2)"
+#   status=$(echo "$status" | grep "Queued")
 
-  if [ -z "${status}" ]; then
-    break  # Exit the loop when ids array is empty
-  fi
+#   if [ -z "${status}" ]; then
+#     break  # Exit the loop when ids array is empty
+#   fi
 
-  ids=($(echo "$status" | sed -n 's/^\([^[:space:]]\+\).*/\1/p'))
+#   ids=($(echo "$status" | sed -n 's/^\([^[:space:]]\+\).*/\1/p'))
 
 
 
-  #dvc queue start
+#   #dvc queue start
 
   
 
-  dvc queue start -j 5
-  sleep 900 # every 15 minutes
-  #parallel --joblog my_joblog.log -j 10 --eta run_exp_with_id {} ::: "${ids[@]}"
-done
+#   dvc queue start -j 5
+#   sleep 900 # every 15 minutes
+#   #parallel --joblog my_joblog.log -j 10 --eta run_exp_with_id {} ::: "${ids[@]}"
+# done
 
 
-#dvc queue start -j 10 # NOTE: Possible race condition on lock > -j 1 leading to failed tasks, also worker not picking up new tasks
-# show progress every 30 minutes because it is time consuming
-#watch -n 1800 echo "Progress" $(dvc queue status | grep "Success\|Failed" | wc -l) "/" $(dvc queue status | wc -l) $(dvc queue status | grep Failed | wc -l) Failed
+dvc queue start -j 10
