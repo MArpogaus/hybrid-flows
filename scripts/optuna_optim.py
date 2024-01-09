05592d34295beef09dff71e6f04313a4e8880819
Julian Jandeleit <julian.jandeleit@htwg-konstanz.de>
2024-01-09 12:31:33 +0000

    fix and parallelize optuna

--- scripts/optuna_optim.py
+++ scripts/optuna_optim.py
@@ -1,55 +1,111 @@
 # requires cmaes package to be present,
 # not automatically installed with conda install optuna!
 
-import json
+import logging
+import os
 import pickle
-import subprocess
+from collections import defaultdict
+from copy import deepcopy
+from types import SimpleNamespace
 
+import mlflow
+import numpy as np
 import optuna
-import yaml
 from optuna.samplers import CmaEsSampler
 from train_benchmark import main
-from types import SimpleNamespace
-from collections import defaultdict
+
+from mctm.utils.pipeline import prepare_pipeline
+
 
 def nested_dictionary():
     return defaultdict(nested_dictionary)
 
-def run_cmd(command: str):
-    process = subprocess.Popen(command, shell=True)
-    process.wait()
+
+def update_nested_dict(original, update):
+    for key, value in update.items():
+        if (
+            isinstance(value, dict)
+            and key in original
+            and isinstance(original[key], dict)
+        ):
+            update_nested_dict(original[key], value)
+        else:
+            original[key] = value
 
 
-dataset = "miniboone"
-distribution = "elementwise_flow"
-model = "unconditional_benchmark"
+# def run_cmd(command: str):
+#     process = subprocess.Popen(command, shell=True)
+#     process.wait()
 
-run_exp = f"dvc exp run {model}@{distribution}-{dataset} -S testmode=true"
-read_results = f"cat results/{model}_{distribution}_{dataset}/metrics.yaml"
+# run_exp = f"dvc exp run {model}@{distribution}-{dataset} -S testmode=true"#
+# read_results = f"cat results/{model}_{distribution}_{dataset}/metrics.yaml"
 # print(run_exp)
 # run_cmd(run_exp)
 # run_cmd(read_results)
 
 
-def run_exp_with_overrides(overrides: dict):
+def build_args(
+    dataset="miniboone",
+    distribution="masked_autoregressive_flow",
+    model="unconditional_benchmark",
+):
     log_level = "info"
     log_file = f"results/{model}_{distribution}_{dataset}/train.log"
     experiment_name = f"{model}_{dataset}"
     stage_name = f"{model}@{distribution}-{dataset}"
     results_path = f"results/{model}_{distribution}_{dataset}"
 
-    args = SimpleNamespace(log_file=log_file, log_level=log_level, test_mode=True, experiment_name=experiment_name, stage_name=stage_name, distribution=distribution, dataset=dataset, results_path=results_path)
+    args = SimpleNamespace(
+        log_file=log_file,
+        log_level=log_level,
+        test_mode=True,
+        experiment_name=experiment_name,
+        stage_name=stage_name,
+        distribution=distribution,
+        dataset=dataset,
+        results_path=results_path,
+    )
+    return args
 
-    history, _, _ = main(args, overrides)
 
-    val_loss = history.history["val_loss"]
+def args_inverse(args):
+    dataset = args.dataset
+    distribution = args.distribution
+    model = args.stage_name.split("@")[0]
+    return dataset, distribution, model
 
+
+args = build_args()
+params = prepare_pipeline(args)
+
+
+def run_exp_with_overrides(overrides: dict):
+    try:
+        exp_params = deepcopy(params)
+        update_nested_dict(exp_params, overrides)
+        logging.info(f"{exp_params=}")
+        history, _, _ = main(
+            args.experiment_name,
+            args.results_path,
+            args.log_file,
+            args.log_level,
+            args.dataset,
+            args.stage_name,
+            args.distribution,
+            exp_params,
+            args.test_mode,
+        )
+
+        min_idx = np.argmin(history.history["val_loss"])
+        val_loss = history.history["val_loss"][min_idx]
+    except:
+        val_loss = 1234567890
     return val_loss
 
-    #cmd = f"dvc exp run {model}@{distribution}-{dataset} -S {overrides}"
-    #print(f"executing {cmd}")
-    #run_cmd(cmd)
-    #with open(f"results/{model}_{distribution}_{dataset}/metrics.yaml", "r") as file:
+    # cmd = f"dvc exp run {model}@{distribution}-{dataset} -S {overrides}"
+    # print(f"executing {cmd}")
+    # run_cmd(cmd)
+    # with open(f"results/{model}_{distribution}_{dataset}/metrics.yaml", "r") as file:
     #    metrics = yaml.safe_load(file)
     #    return float(metrics["val_loss"])
 
@@ -58,40 +114,67 @@ study = optuna.create_study(sampler=CmaEsSampler(), directions=["minimize"])
 
 
 def objective(trial):
-    #SEED
-    
+    # SEED
+
+    dataset, distribution, model = args_inverse(args)
+
     batch_size = trial.suggest_int("batch_size", 32, 512)
-    #learning_rate = trial.suggest_float(key_learning_rate, 0.001, 0.01)
-    #patience = trial.suggest_int(key_patience, 5, 10)
-    # n_hidden_layers = 3
+    learning_rate = trial.suggest_float("key_learning_rate", 0.001, 0.01)
+    patience = trial.suggest_int("key_patience", 5, 10)
+    layers = [trial.suggest_int(f"hidden_units_{i}", 64, 1028) for i in [1, 2, 3]]
 
     overrides = nested_dictionary()
-    overrides[f"{model}_distributions"][distribution][dataset]["fit_kwds"]["batch_size"] = batch_size
+    # TODO: this is equal to exp name so replace model_distributions
+    overrides[f"{model}_distributions"][distribution][dataset]["fit_kwds"][
+        "batch_size"
+    ] = batch_size
+    overrides[f"{model}_distributions"][distribution][dataset]["fit_kwds"][
+        "learning_rate"
+    ] = learning_rate
+    overrides[f"{model}_distributions"][distribution][dataset]["fit_kwds"][
+        "lr_patience"
+    ] = patience
+    overrides[f"{model}_distributions"][distribution][dataset]["parameter_kwds"][
+        "hidden_units"
+    ] = layers
     val_loss = run_exp_with_overrides(overrides)
     return val_loss
 
-import time
-start_time = time.time()
-study.optimize(objective, n_trials=3, n_jobs=1)
-end_time = time.time()
 
-optimum = study.best_params
-
-print(
-    (
-        f"finished hyperparameter optimization with optimum:\n{optimum}"
-        "\n writing result to _data.json_."
-    )
+mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
+mlflow.set_experiment(
+    f"optuna-optim-{args_inverse(args)[2]}_{args_inverse(args)[1]}_{args_inverse(args)[0]}"
 )
+with mlflow.start_run(nested=True) as run:
+    import time
 
+    start_time = time.time()
+    study.optimize(objective, n_trials=1000, n_jobs=3)  # to many jobs cause freezing
+    end_time = time.time()
 
-with open("data.json", "w") as f:
-    json.dump(optimum, f)
+    optimum = study.best_params
 
+    print(
+        (
+            f"finished hyperparameter optimization with optimum:\n{optimum}"
+            "\n writing result to _data.json_."
+        )
+    )
+    mlflow.log_dict(optimum, "optimal_parameters.json")
 
-with open("study.pickle", "wb") as handle:
-    pickle.dump(study, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open("study.pickle", "wb") as handle:
+        pickle.dump(study, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    mlflow.log_artifact("study.pickle")
 
-print(study.best_trial)
-print(study.best_value)
-print(end_time - start_time)
+    if optuna.visualization.is_available():
+        fig = optuna.visualization.plot_optimization_history(study)
+        mlflow.log_figure(fig, "optim_history.html")
+    # mlflowlog -> bernsteinflow/cml/hp_
+
+    # with open("data.json", "w") as f:
+    #     json.dump(optimum, f)
+
+    print("\n\n")
+    print(f"best trial: {study.best_trial}")
+    print(f"result value: {study.best_value}")
+    print(f"optimizer took {end_time - start_time} seconds")
