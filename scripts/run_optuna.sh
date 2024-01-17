10a31690cfe69e9fbadc245ad913a296db3981a7
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2024-01-17 14:12:48 +0100

    adds bash script to run the optuna scrip

---
+++ scripts/run_optuna.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -eu
+set -o pipefail
+
+# Function to display the script usage
+usage() {
+	echo "Usage: $0 [-h] STAGE_NAME DISTRIBUTION DATASET STUDY_NAME JOBS"
+	echo "    -h               Display this help message"
+	echo "    STAGE_NAME       Name of the stage"
+	echo "    DISTRIBUTION     Distribution type"
+	echo "    DATASET          Dataset name"
+	echo "    STUDY_NAME       Study name"
+	echo "    TRAILS           Number trials per jobs"
+	echo "    JOBS             Number of jobs"
+	exit 1
+}
+
+# Parse command line arguments
+while getopts ":h" option; do
+	case "$option" in
+	h)
+		usage
+		;;
+	*)
+		echo "Error: Invalid option -$OPTARG"
+		usage
+		;;
+	esac
+done
+
+# Set variables based on provided arguments
+shift $((OPTIND - 1))
+if [ "$#" -ne 6 ]; then
+	echo "Error: 6 arguments required."
+	usage
+fi
+
+STAGE_NAME=$1
+DISTRIBUTION=$2
+DATASET=$3
+STUDY_NAME=$4
+TRIALS=$5
+JOBS=$6
+
+DB_NAME=${STUDY_NAME//-/_}.db
+
+RESULTS_PATH=results/optuna/${STAGE_NAME}_${DISTRIBUTION}_${DATASET}
+PARAMETER_SPACE_DEFINITION_FILE=optuna/parameter_space_definition_${STAGE_NAME}_${DISTRIBUTION}_${DATASET}.yaml
+
+# Create study
+if [ ! -e $DB_NAME ]; then
+	optuna create-study --study-name "test-study" --storage sqlite:///$DB_NAME --directions minimize
+fi
+
+run_optuna_script() {
+	python scripts/optuna_new.py \
+		--experiment-name optuna \
+		--stage-name ${STAGE_NAME}@${DISTRIBUTION}-${DATASET} \
+		--distribution ${DISTRIBUTION} \
+		--dataset ${DATASET} \
+		--results_path ${RESULTS_PATH} \
+		--parameter_space_definition_file ${PARAMETER_SPACE_DEFINITION_FILE} \
+		--study-name=${STUDY_NAME} \
+		--load-study=sqlite:///${DB_NAME} \
+		--n-trials=${TRIALS} \
+		--seed=$1
+}
+
+for ((seed=1; seed<=$JOBS; seed++)); do
+    run_optuna_script $seed &
+done
+
+wait  # Wait for all the background jobs to finish
