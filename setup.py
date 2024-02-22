f6c745665db6780e59c3df0bd8a8287251c190c3
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2024-02-22 14:09:07 +0100

    chore: delete unneeded files

--- setup.py
+++
@@ -1,9 +0,0 @@
-"""setup module."""
-# Only provided for legacy compatibility
-# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
-#
-# Metadate is specified pyproject.toml
-
-from setuptools import setup
-
-setup()
