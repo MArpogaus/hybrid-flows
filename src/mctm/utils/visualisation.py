bf4b81fa7eae11b0e000c64509100f34ba6c72e1
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2024-08-22 13:53:18 +0200

    feat: add plot functions for malnutrition data and models

--- src/mctm/utils/visualisation.py
+++ src/mctm/utils/visualisation.py
@@ -1,19 +1,19 @@
-"""Visualization utils."""
-
 # -*- time-stamp-pattern: "changed[\s]+:[\s]+%%$"; -*-
-# AUTHOR INFORMATION ###########################################################
+# %% Author ####################################################################
 # file    : visualisation.py
 # author  : Marcel Arpogaus <znepry.necbtnhf@tznvy.pbz>
 #
-# created : 2023-08-22 13:19:25 (Marcel Arpogaus)
-# changed : 2023-08-22 13:19:25 (Marcel Arpogaus)
-# DESCRIPTION ##################################################################
-# ...
-# LICENSE ######################################################################
-# ...
-################################################################################
+# created : 2024-08-22 12:12:12 (Marcel Arpogaus)
+# changed : 2024-08-22 13:53:13 (Marcel Arpogaus)
+
+# %% License ###################################################################
+
+# %% Description ###############################################################
+"""Visualization utils."""
+
+# %% imports ###################################################################
 import time
-from typing import Any, Tuple, Union
+from typing import Any, List, Tuple, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,25 +21,30 @@ import pandas as pd
 import seaborn as sns
 import tensorflow as tf
 import tensorflow_probability as tfp
+from matplotlib.figure import Figure
+from tensorflow_probability import distributions as tfd
+
+from mctm.models import DensityRegressionModel
 
 
-def _joint_kde_plot(data: pd.DataFrame, x: str, y: str, **kwargs) -> plt.Figure:
+# %% private functions #########################################################
+def _joint_kde_plot(data: pd.DataFrame, x: str, y: str, **kwargs: Any) -> Figure:
     """Generate a joint KDE plot of two variables from a dataset.
 
     Parameters
     ----------
-    data
+    data : pd.DataFrame
         The dataset containing the variables to be plotted.
-    x
+    x : str
         The name of the variable to plot on the x-axis.
-    y
+    y : str
         The name of the variable to plot on the y-axis.
     kwargs
-        Keyword arguments passed to `sns.jointplot`
+        Keyword arguments passed to `sns.jointplot`.
 
     Returns
     -------
-    plt.Figure
+    Figure
         The generated plot figure.
 
     """
@@ -66,6 +71,71 @@ def _joint_kde_plot(data: pd.DataFrame, x: str, y: str, **kwargs) -> plt.Figure:
     return g.figure
 
 
+def _plot_grid(data: pd.DataFrame, **kwargs: Any) -> Figure:
+    """Plot sns.PairGrid.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Dataframe containing the data to plot.
+    kwargs
+        Additional arguments for sns.PairGrid.
+
+    Returns
+    -------
+    Figure
+        The figure object resulting from the PairGrid.
+
+    """
+    sns.set_theme(style="white")
+    g = sns.PairGrid(data, diag_sharey=False, **kwargs)
+    g.map_upper(sns.scatterplot, s=10, alpha=0.5)
+    g.map_lower(sns.kdeplot)
+    g.map_diag(sns.kdeplot, lw=2)
+
+    return g.figure
+
+
+def _get_malnutrition_samples_df(
+    model: DensityRegressionModel,
+    x: tf.Tensor,
+    y: np.ndarray,
+    seed: int,
+    targets: List[str],
+) -> pd.DataFrame:
+    """Generate a DataFrame with model samples and data for malnutrition analysis.
+
+    Parameters
+    ----------
+    model : DensityRegressionModel
+        The trained density regression model.
+    x : tf.Tensor
+        Input features for the model.
+    y : np.ndarray
+        Target values.
+    seed : int
+        Random seed for sampling.
+    targets : list
+        List of target variable names.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame containing model samples and data with source and cage information.
+
+    """
+    df_data = pd.DataFrame(y, columns=targets).assign(source="data").assign(cage=x)
+    df_model = (
+        pd.DataFrame(model(x).sample(seed=seed).numpy().squeeze(), columns=targets)
+        .assign(source="model")
+        .assign(cage=x)
+    )
+    df = pd.concat([df_data, df_model])
+
+    return df
+
+
+# %% public functions ##########################################################
 def get_figsize(
     width: Union[str, float], fraction: float = 1, subplots: Tuple[int, int] = (1, 1)
 ) -> Tuple[float, float]:
@@ -73,15 +143,16 @@ def get_figsize(
 
     Parameters
     ----------
-    width
+    width : Union[str, float]
         Document width in points, or string of predefined document type.
-    fraction
-        Fraction of the width which you wish the figure to occupy.
-    subplots
-        The number of rows and columns of subplots.
+    fraction : float, optional
+        Fraction of the width which you wish the figure to occupy, by default 1.
+    subplots : Tuple[int, int], optional
+        The number of rows and columns of subplots, by default (1, 1).
 
     Returns
     -------
+    Tuple[float, float]
         Dimensions of the figure in inches.
 
     """
@@ -114,8 +185,8 @@ def setup_latex(fontsize: int = 10) -> None:
 
     Parameters
     ----------
-    fontsize
-        Font size to use for labels and text in the plots.
+    fontsize : int, optional
+        Font size to use for labels and text in the plots, by default 10.
 
     """
     tex_fonts = {
@@ -138,23 +209,28 @@ def setup_latex(fontsize: int = 10) -> None:
 
 
 def plot_2d_data(
-    X: np.ndarray, Y: np.ndarray, plot_kwargs: dict = dict(s=5, alpha=0.7), **kwargs
-) -> plt.Figure:
+    X: np.ndarray,
+    Y: np.ndarray,
+    plot_kwargs: dict = dict(s=5, alpha=0.7),
+    **kwargs: Any,
+) -> Figure:
     """Create a 2D scatter plot for binary labeled data.
 
     Parameters
     ----------
-    X
+    X : np.ndarray
         Data points to plot.
-    Y
+    Y : np.ndarray
         Binary labels for the data points.
-    plot_kwargs
-        Keyword arguments passed to `plt.scatter`
+    plot_kwargs : dict, optional
+        Keyword arguments passed to `plt.scatter`,
+        by default dict(s=5, alpha=0.7).
     kwargs
-        Keyword arguments passed to `plt.figure`
+        Keyword arguments passed to `plt.figure`.
 
     Returns
     -------
+    Figure
         The generated plot figure.
 
     """
@@ -175,23 +251,27 @@ def plot_2d_data(
 
 
 def plot_samples(
-    dist: tfp.distributions.Distribution, data: np.ndarray, seed: int = 1, **kwargs
-) -> plt.Figure:
+    dist: tfd.Distribution,
+    data: np.ndarray,
+    seed: int = 1,
+    **kwargs: Any,
+) -> Figure:
     """Create a joint KDE plot of data samples and a probability distribution.
 
     Parameters
     ----------
-    dist
+    dist : tfd.Distribution
         A TensorFlow probability distribution.
-    data
+    data : np.ndarray
         Data samples to compare with the distribution.
-    seed
-        Random seed for generating samples.
+    seed : int, optional
+        Random seed for generating samples, by default 1.
     kwargs
-        Keyword arguments passed to `_joint_kde_plot`
+        Keyword arguments passed to `_joint_kde_plot`.
 
     Returns
     -------
+    Figure
         The generated plot figure.
 
     """
@@ -214,34 +294,42 @@ def plot_samples(
 
 
 def plot_flow(
-    dist: tfp.bijectors.Bijector, x: np.ndarray, y: np.ndarray, seed: int = 1, **kwargs
-) -> Tuple[plt.Figure, plt.Figure, plt.Figure]:
+    dist: tfp.bijectors.Bijector,
+    x: np.ndarray,
+    y: np.ndarray,
+    seed: int = 1,
+    **kwargs: Any,
+) -> Tuple[Figure, Figure, Figure]:
     """Create joint KDE plots to visualize data transformation through a flow.
 
     Parameters
     ----------
-    dist
+    dist : tfp.bijectors.Bijector
         A TensorFlow probability bijector representing the data transformation.
-    x
+    x : np.ndarray
         Input data to the flow transformation.
-    y
+    y : np.ndarray
         Transformed data after applying the flow.
-    seed
-        Random seed for generating samples.
+    seed : int, optional
+        Random seed for generating samples, by default 1.
     kwargs
-        Keyword arguments passed to `_joint_kde_plot`
+        Keyword arguments passed to `_joint_kde_plot`.
 
     Returns
     -------
-        Figures of the joint KDE plots for data transformation.
+    Tuple[Figure, Figure, Figure]
+        Figures of the joint KDE plots for:
+            - Data transformation.
+            - Independent components.
+            - Base distribution.
 
     """
     base_dist = dist.distribution.distribution
     columns = ["$y_1$", "$y_2$", "$z_{1,1}$", "$z_{1,2}$", "$z_{2,1}$", "$z_{2,2}$"]
-    y = tf.convert_to_tensor(y, dtype=tf.float32)
+    y_tensor = tf.convert_to_tensor(y, dtype=tf.float32)
 
     # inverse flow
-    z1 = dist.bijector.inverse(y)
+    z1 = dist.inverse(y_tensor)
     z2 = base_dist.bijector.inverse(z1)
     df_inv = pd.DataFrame(np.concatenate([y, z1, z2], 1), columns=columns).assign(
         label=x, source="data"
@@ -250,7 +338,7 @@ def plot_flow(
     # forward flow (bnf in inverted)
     z2 = base_dist.distribution.sample(y.shape, seed=seed)
     z1 = base_dist.bijector.forward(z2)
-    yy = dist.bijector.forward(z1)
+    yy = dist.forward(z1)
     df_fwd = pd.DataFrame(
         np.concatenate([yy, z1, z2], 1),
         columns=columns,
@@ -270,30 +358,42 @@ def plot_flow(
 
 
 def plot_copula_function(
-    dist: Any, y: np.ndarray, kind: str, x_min: float, x_max: float, n: int, **kwargs
-) -> plt.Figure:
+    dist: Any,
+    y: np.ndarray,
+    kind: str,
+    x_min: float,
+    x_max: float,
+    n: int,
+    **kwargs: Any,
+) -> Figure:
     """Plot copula function either as surface or contour.
 
     Parameters
     ----------
-    dist
+    dist : Any
         Copula distribution from TensorFlow Probability.
-    y
+    y : np.ndarray
         Data points for the Copula function.
-    kind
+    kind : str
         Type of plot to generate ('surface' or 'contour').
-    x_min
+    x_min : float
         Minimum value for the x-axis.
-    x_max
+    x_max : float
         Maximum value for the x-axis.
-    n
+    n : int
         Number of points to generate between x_min and x_max.
     kwargs
         Keyword arguments passed to plot function.
 
     Returns
     -------
+    Figure
         Generated Matplotlib figure.
+
+    Raises
+    ------
+    ValueError
+        If an unsupported plot kind is provided.
 
     """
     dist_z1 = dist.distribution
@@ -353,3 +453,164 @@ def plot_copula_function(
         return fig
     else:
         raise ValueError(f"Unsupported plot kind: {kind}")
+
+
+def plot_malnutrition_data(
+    *data: Tuple[Tuple[tf.Tensor, ...], ...],
+    targets: List[str],
+    covariates: List[str],
+    seed: int,
+    frac: float,
+    **kwargs: Any,
+) -> Figure:
+    """Plot malnutrition data.
+
+    Parameters
+    ----------
+    data : tuple
+        Data containing the features and targets.
+    targets : list
+        List of target variable names.
+    covariates : list
+        List of covariate variable names.
+    seed : int
+        Random seed for sampling.
+    frac : float
+        Fraction of data to sample.
+    **kwargs
+        Additional plotting arguments.
+
+    Returns
+    -------
+    Figure
+        The resulting figure from the plot.
+
+    """
+    df = pd.DataFrame(
+        np.concatenate([data[0][1], data[0][0]], -1),
+        columns=targets + covariates,
+    )
+    return _plot_grid(
+        df.groupby(covariates).sample(frac=frac, random_state=seed),
+        vars=targets,
+        **kwargs,
+    )
+
+
+def plot_malnutrition_samples(
+    model: DensityRegressionModel,
+    x: tf.Tensor,
+    y: np.ndarray,
+    seed: int,
+    targets: List[str],
+    **kwargs: Any,
+) -> Figure:
+    """Generate and plot samples from a malnutrition model.
+
+    Parameters
+    ----------
+    model : DensityRegressionModel
+        The trained density regression model for malnutrition.
+    x : tf.Tensor
+        Input features for the model.
+    y : np.ndarray
+        Target values.
+    seed : int
+        Random seed for sampling.
+    targets : list
+        List of target variable names.
+    **kwargs
+        Additional plotting arguments passed to `_plot_grid`.
+
+    Returns
+    -------
+    Figure
+        The resulting figure from the plot.
+
+    """
+    df = _get_malnutrition_samples_df(model=model, x=x, y=y, seed=seed, targets=targets)
+    fig = _plot_grid(
+        df.loc[df.source == "model"]
+        .groupby("cage")
+        .sample(frac=0.2, random_state=seed),
+        vars=targets,
+        hue="cage",
+        **kwargs,
+    )
+    return fig
+
+
+def plot_marginal_cdf_and_pdf(
+    model: DensityRegressionModel,
+    covariates: List[int],
+    target_names: List[str],
+    palette: str = "mako_r",
+    y_range: Tuple[float, float] = (-4, 4),
+    y_samples: int = 100,
+    n_bijectors: int = 2,
+    **kwargs: Any,
+) -> Figure:
+    """Plot the marginal CDF and PDF for a given model and dataset.
+
+    Parameters
+    ----------
+    model : DensityRegressionModel
+        The density regression model.
+    covariates : List[int]
+        A list of covariates to plot.
+    target_names : List[str]
+        A list of target names.
+    palette : str, optional
+        The color palette to use, by default "mako_r".
+    y_range : Tuple[float, float], optional
+        The range of y values to plot, by default (-4, 4).
+    y_samples : int, optional
+        The number of y samples to use, by default 100.
+    n_bijectors : int, optional
+        The number of bijectors to use, by default 2.
+    **kwargs
+        Additional keyword arguments to pass to the plotting function.
+
+    Returns
+    -------
+    Figure
+        The matplotlib figure object.
+
+    """
+    colors = sns.color_palette(palette, as_cmap=True)(
+        np.linspace(0, 1, len(covariates))
+    ).tolist()
+    joint_dist = model(tf.convert_to_tensor(covariates, dtype=model.dtype)[..., None])
+    marginal_dist = tfd.TransformedDistribution(
+        distribution=tfd.Normal(0, 1),
+        bijector=tfp.bijectors.Invert(
+            tfp.bijectors.Chain(joint_dist.bijector.bijector.bijectors[-n_bijectors:])
+        ),
+    )
+
+    y = np.linspace(y_range[0], y_range[1], y_samples)[..., None, None]
+
+    cdf = marginal_dist.cdf(y).numpy()
+    pdf = marginal_dist.prob(y).numpy()
+    fig, axs = plt.subplots(2, len(target_names), sharey="row", sharex=True, **kwargs)
+
+    for i, c in enumerate(target_names):
+        axs[0, i].set_prop_cycle("color", colors)
+        axs[0, i].plot(y.flatten(), cdf[..., i], label=covariates, lw=0.5)
+        axs[1, i].set_prop_cycle("color", colors)
+        axs[1, i].plot(y.flatten(), pdf[..., i], label=covariates, lw=0.5)
+        axs[1, i].set_xlabel(f"y={c}")
+        if i == 0:
+            axs[0, i].legend(
+                covariates,
+                title="Age",
+                loc="right",
+                fontsize=8,
+                frameon=False,
+            )
+
+    axs[0, 0].set_ylabel(r"$F(y|\text{age})$")
+    axs[1, 0].set_ylabel(r"$f(y|\text{age})$")
+
+    fig.tight_layout(w_pad=0)
+    return fig
