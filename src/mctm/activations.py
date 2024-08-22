1123cf9ea4f47aabdb6d50d6651273434a6b6d82
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2024-08-22 13:59:10 +0200

    feat: add function to constrain mctm lambda matrix

--- src/mctm/activations.py
+++ src/mctm/activations.py
@@ -1,18 +1,16 @@
 # -*- time-stamp-pattern: "changed[\s]+:[\s]+%%$"; -*-
-# AUTHOR INFORMATION ##########################################################
-# file    : __init__.py
-# author  : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
+# %% Author ####################################################################
+# file    : activations.py
+# author  : Marcel Arpogaus <znepry.necbtnhf@tznvy.pbz>
 #
-# created : 2022-03-10 15:39:04 (Marcel Arpogaus)
-# changed : 2024-03-26 18:28:45 (Marcel Arpogaus)
-# DESCRIPTION #################################################################
-# ...
-# LICENSE #####################################################################
-# ...
-###############################################################################
+# created : 2024-08-22 13:55:15 (Marcel Arpogaus)
+# changed : 2024-08-22 13:59:03 (Marcel Arpogaus)
+
+# %% Description ###############################################################
 """Defines functions to constrain NN outputs."""
 
-from typing import Callable
+# %% imports ###################################################################
+from typing import Callable, Dict
 
 import tensorflow as tf
 from tensorflow_probability.python.internal import (
@@ -21,6 +19,35 @@ from tensorflow_probability.python.internal import (
 )
 
 
+# %% private functions #########################################################
+def _create_lambda_matrix(x: tf.Tensor) -> tf.Tensor:
+    """Create a lower triangular lambda matrix from `x` with a diagonal of ones.
+
+    Parameters
+    ----------
+    x : tf.Tensor
+        Input tensor containing elements to fill the lower triangular matrix.
+
+    Returns
+    -------
+    tf.Tensor
+        Resulting lambda matrix.
+
+    """
+    num_elements = x.shape[-1]
+    n = tf.cast(
+        tf.math.ceil(tf.math.sqrt(2.0 * tf.cast(num_elements, tf.float32))), tf.int32
+    )
+    diag_ones = tf.linalg.diag(tf.ones((n,), dtype=x.dtype))
+    tri_lower_indices = tf.where(
+        tf.linalg.band_part(tf.ones((n, n)), -1, 0) - tf.eye(n) > 0
+    )
+    lower_tri_elements = tf.scatter_nd(tri_lower_indices, x, shape=(n, n))
+    mat = diag_ones + lower_tri_elements
+    return mat
+
+
+# %% functions #################################################################
 def get_thetas_constrain_fn(
     low: float = -4.0,
     high: float = 4.0,
@@ -28,24 +55,25 @@ def get_thetas_constrain_fn(
     allow_flexible_bounds: bool = False,
     min_slope: float = 1e-5,
 ) -> Callable[[tf.Tensor], tf.Tensor]:
-    """Create a function to constrain the Bernstein coeficents within specified bounds.
+    """Return function to constrain the Bernstein coefficients within specified bounds.
 
     Parameters
     ----------
-    low
+    low : float, optional
         The lower bound for the first theta, by default -4.0.
-    high
+    high : float, optional
         The upper bound for the last theta, by default 4.0.
-    bounds
+    bounds : str, optional
         The type of bounds to apply, either "smooth", "identity", or others,
         by default "smooth".
-    allow_flexible_bounds
+    allow_flexible_bounds : bool, optional
         If True, allows dynamic adjustment of bounds based on input, by default False.
-    min_slope
+    min_slope : float, optional
         The minimum slope to ensure numeric stability, by default 1e-5.
 
     Returns
     -------
+    Callable[[tf.Tensor], tf.Tensor]
         A function that takes a tensor and returns it constrained according to the
         specified options.
 
@@ -54,12 +82,10 @@ def get_thetas_constrain_fn(
     def constrain_fn(diff: tf.Tensor) -> tf.Tensor:
         dtype = dtype_util.common_dtype([diff], dtype_hint=tf.float32)
         diff = tensor_util.convert_nonref_to_tensor(diff, name="diff", dtype=dtype)
-
         if low is not None:
             low_theta = tensor_util.convert_nonref_to_tensor(
                 low, name="low", dtype=dtype
             ) * tf.ones_like(diff[..., :1])
-
             if allow_flexible_bounds:
                 low_theta -= tf.math.softplus(diff[..., :1])
                 diff = diff[..., 1:]
@@ -72,7 +98,6 @@ def get_thetas_constrain_fn(
             high_theta = tensor_util.convert_nonref_to_tensor(
                 high, name="high", dtype=dtype
             )
-
             if allow_flexible_bounds:
                 high_theta += tf.math.softplus(diff[..., :1])
                 diff = diff[..., 1:]
@@ -109,10 +134,7 @@ def get_thetas_constrain_fn(
                 axis=-1,
             )
         else:
-            c = tf.concat(
-                (low_theta, diff_positive),
-                axis=-1,
-            )
+            c = tf.concat((low_theta, diff_positive), axis=-1)
 
         thetas_constrained = tf.cumsum(c, axis=-1, name="theta")
         return thetas_constrained
@@ -122,22 +144,23 @@ def get_thetas_constrain_fn(
 
 def get_spline_param_constrain_fn(
     nbins: int, interval_width: float, min_bin_width: float, min_slope: float
-) -> Callable[[tf.Tensor], dict]:
+) -> Callable[[tf.Tensor], Dict[str, tf.Tensor]]:
     """Create a function to constrain the parameters of a spline bijector.
 
     Parameters
     ----------
-    nbins
+    nbins : int
         Number of bins in the spline.
-    interval_width
+    interval_width : float
         Total width of the interval to be covered by the bins.
-    min_bin_width
+    min_bin_width : float
         The minimum width that any bin can have.
-    min_slope
+    min_slope : float
         The minimum slope to ensure numeric stability.
 
     Returns
     -------
+    Callable[[tf.Tensor], Dict[str, tf.Tensor]]
         A function that takes unconstrained parameters and returns them constrained
         within specified bounds, organized into a dictionary.
 
@@ -152,7 +175,7 @@ def get_spline_param_constrain_fn(
     def _slopes(x: tf.Tensor) -> tf.Tensor:
         return tf.math.softplus(x) + min_slope
 
-    def constrain_fn(unconstrained_parameters: tf.Tensor) -> dict:
+    def constrain_fn(unconstrained_parameters: tf.Tensor) -> Dict[str, tf.Tensor]:
         bin_widths, bin_heights, knot_slopes = tf.split(
             unconstrained_parameters, [nbins, nbins, nbins - 1], axis=-1
         )
@@ -164,3 +187,24 @@ def get_spline_param_constrain_fn(
         }
 
     return constrain_fn
+
+
+def lambda_parameters_constraint_fn(
+    x: tf.Tensor,
+) -> tf.linalg.LinearOperatorLowerTriangular:
+    """Create lower triangular matrix with ones on the diagonal.
+
+    Parameters
+    ----------
+    x : tf.Tensor
+        Input tensor containing the elements for the lower triangular matrix.
+
+    Returns
+    -------
+    tf.linalg.LinearOperatorLowerTriangular
+        Linear operator representing the constrained lower triangular matrix.
+
+    """
+    return tf.linalg.LinearOperatorLowerTriangular(
+        tf.vectorized_map(_create_lambda_matrix, x)
+    )
