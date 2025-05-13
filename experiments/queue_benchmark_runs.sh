1ece2ba1d7906d8ce33a58f11b012cbbc307d2eb
slurm@invalid.com <slurm>
2025-05-13 16:33:38 +0200

    update benchmark exp script

--- experiments/queue_benchmark_runs.sh
+++ experiments/queue_benchmark_runs.sh
@@ -7,13 +7,19 @@ dvc gc --all-experiments -a -f
 git gc --aggressive
 
 MODELS=$(dvc status eval-benchmark | grep -oP '@dataset\d-\K.*(?=:)' | sort | uniq)
+EXP_NAME="benchmark-seeds-$(date -I)"
 
 for dataset in {0,4}; do
 	for model in $MODELS; do
-		exp_name="benchmark-seeds-$(date -I)"
-		dvc exp run --force --queue -S "seed=range(1,21)" \
-			-S "train-benchmark-experiment-name=$exp_name" \
-			-S "eval-benchmark-experiment-name=$exp_name" \
-			eval-benchmark@dataset${dataset}-${model}
+		for seed in {1..20}; do
+			dvc exp run --force --temp \
+				-S "seed=$seed" \
+				-S "train-benchmark-experiment-name=$EXP_NAME" \
+				-S "eval-benchmark-experiment-name=$EXP_NAME" \
+				eval-benchmark@dataset${dataset}-${model} &
+                        sleep 15
+		done
 	done
 done
+
+wait
